4e532a02aab59481560fb0294d1560ddeaa23d6a
kudo <kuasehujikolp@yahoo.co.jp>
2026-05-13 03:02:54 +0900

    ggg

--- src/assets/test_questions/manifest.ts
+++ src/assets/test_questions/manifest.ts
@@ -8,7 +8,7 @@ export const questionSetCounts = {
     '/dns/dns2': 15,
     '/dynamodb/dynamodb1': 14,
     '/dynamodb/dynamodb2': 5,
-    '/s3/s3_1': 39,
+    '/s3/s3_1': 47,
     '/s3/s3_2': 1,
     '/ssa': 20,
 } as const
