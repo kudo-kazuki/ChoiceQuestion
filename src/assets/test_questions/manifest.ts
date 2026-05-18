3226b8ba3be535a27b8a0a0c277fbd003ff463e7
kudo <kuasehujikolp@yahoo.co.jp>
2026-05-19 00:12:08 +0900

    d

--- src/assets/test_questions/manifest.ts
+++ src/assets/test_questions/manifest.ts
@@ -9,7 +9,7 @@ export const questionSetCounts = {
     '/dynamodb/dynamodb1': 14,
     '/dynamodb/dynamodb2': 5,
     '/lambda/lambda1': 116,
-    '/lambda/lambda2': 105,
+    '/lambda/lambda2': 115,
     '/s3/s3_1': 54,
     '/s3/s3_2': 77,
     '/ssa': 20,
